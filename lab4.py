60049e42b372ba713487f583e73f94c6b08ff0f1
Orishi <Orishi@XPS-NINJA>
2024-01-25 09:07:51 +0200

    Lab 6

--- lab4.py
+++ lab4.py
@@ -1,5 +1,9 @@
 # Lab 1,2 - complete
 # Lab 3: 
+from time import sleep
+from random import randint
+
+
 
 my_details = ['ori','40','ori@email.com','05412345678']
 print(f'\nThese are my details:\n####################\n')
@@ -20,6 +24,41 @@ bank['yair'] = sum
 print(len(bank.items()))
 if 'idan' in bank:
     print('idan is here')
+
+#Lab 6:
+
+def raise_by_power():
+    return()
+def ip_list():
+    return()
+def dict_dns():
+    return()
+def check_palindrome():
+    return()
+
+def menu ():
+    message = (f'\nMenue:\n#########\n\n')
+    message += (f'1) Enter a number to raise by power of 3.\n')
+    message += (f'2) Insert IPs to a list.\n')
+    message += (f'3) Insert DNS to a dictionary.\n')
+    message += (f'4) Check Palindrome\n')
+    message += (f'\n To quit press `q`:\n')
+    pick = 0
+    while pick != 'q':
+        pick = input(message)
+        if pick == 1:
+            raise_by_power()
+        elif pick == 2:
+            ip_list()
+        elif pick == 3:
+            dict_dns()
+        elif pick == 4:
+            check_palindrome()
+        
+
+menu()
+
+
 
 
 
